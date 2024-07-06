c52167dcdf4f9a0f6f01bf8e5b9f2ec4eb4934ca
yanoshi-1009 <y.idakko@gmail.com>
2024-07-06 18:14:01 +0900

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,41 +1,12 @@
 const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = {
-  entry: { index: "./src/js/index.js" },
+  entry: { index: "./src/js/main.js" },
   resolve: {
     alias: {
       modules: __dirname + "/node_modules",
       common: __dirname + "/common"
     }
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(jsx?|tsx?)$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"]
-      },
-      {
-        test: /\.(sa|sc|c)ss$/,
-        use: [
-          "style-loader",
-          {
-            loader: "css-loader",
-            options: { url: false }
-          },
-          {
-            loader: "sass-loader",
-            options: {
-              implementation: require("sass")
-            }
-          }
-        ]
-      },
-      {
-        test: /.(png|jpg|jpeg|gif|svg|woff|woff2|eot|ttf)(\?v=\d+\.\d+\.\d+)?$/i,
-        type: "asset"
-      }
-    ]
   },
   optimization: {
     minimizer: [
